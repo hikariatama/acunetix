286377d1ca4c3a2e85eda85b06d97a20d53410ca
Dan <innocoffee@ya.ru>
2023-01-21 23:15:40 +0300

    Change StatusStatisticEntry

--- acunetix/schema/status_statistic_entry.py
+++ acunetix/schema/status_statistic_entry.py
@@ -1,16 +1,39 @@
 import dataclasses
+import typing
 
 
 @dataclasses.dataclass
 class StatusStatisticEntry:
-    number_of_runs: int
-    operation_name: str
-    total_duration: int
-    average_duration: int
+    number_of_runs: typing.Optional[int] = None
+    operation_name: typing.Optional[str] = None
+    total_duration: typing.Optional[int] = None
+    average_duration: typing.Optional[int] = None
+    location_name: typing.Optional[str] = None
+    number_of_requests: typing.Optional[int] = None
+    total_duration: typing.Optional[int] = None
+    average_duration: typing.Optional[int] = None
 
     @classmethod
     def from_dict(cls, data: dict):
         return cls(**data)
 
     def to_json(self):
-        return dataclasses.asdict(self)
+        data = dataclasses.asdict(self)
+        if data["number_of_runs"] is None:
+            data.pop("number_of_runs")
+        if data["operation_name"] is None:
+            data.pop("operation_name")
+        if data["total_duration"] is None:
+            data.pop("total_duration")
+        if data["average_duration"] is None:
+            data.pop("average_duration")
+        if data["location_name"] is None:
+            data.pop("location_name")
+        if data["number_of_requests"] is None:
+            data.pop("number_of_requests")
+        if data["total_duration"] is None:
+            data.pop("total_duration")
+        if data["average_duration"] is None:
+            data.pop("average_duration")
+
+        return data
